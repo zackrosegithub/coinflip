b19dfac0e411921a9ce0a7818fb1b114ce6fc58f
Zack Rose <zackrose21@gmail.com>
2020-06-14 12:07:59 +0100

    fini

--- src/CoinContainer.js
+++ src/CoinContainer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { choice } from "./helpers";
+import Coin from './coin'
 
 class CoinContainer extends Component {
     static defaultProps = {
         coins: [
             {side: 'heads', imgSrc: "https://tinyurl.com/react-coin-heads-jpg"},
-            {side: 'tails', imgSrc: "https://tinyurl.com/react-coin-heads-jpg"}    
-        ]
+            {side: 'tails', imgSrc: "https://tinyurl.com/react-coin-tails-jpg"}    
+        ] 
     };
     constructor(props) {
         super(props);
@@ -36,8 +37,9 @@ class CoinContainer extends Component {
         return (
             <div className="CoinContainer">
             <h2>Let's Flip a Coin!</h2>
-            <button onClick={this.handleClick}>Flip Me!</button>
+            {this.state.currCoin && <Coin info = {this.state.currCoin}/>}
             <p>Out of {this.state.nFlips} flips, there have been {this.state.nHeads} heads and {this.state.nTails} tails</p>
+            <button onClick={this.handleClick}>Flip Me!</button>
             </div>
         )
     }
